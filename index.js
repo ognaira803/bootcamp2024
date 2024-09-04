5e4d6a74bb9387cce985824caa122a2de1ce26ff
ognaira803 <ognaira803@gmail.com>
2024-09-04 11:31:23 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const x = 5
+let y = 5
+   y = 10 
+// data types - primitive and non primitive
+// number
+ let z =5, a =1, b =3
+
+firstName = "chinonso \nemmanuel"
+lastName = "Emmanuel"
+let user = firstName + " " + lastName
+ const bool = 5 < 6
+ const num = null
+ let v 
+
+// object
+let studentData ={
+   studentName: 'nonso',
+   class: 'jss1',
+   age: '12',
+   gender: 'male',
+}
+
+console.log(studentData.studentName)
+
+// array
+let fruits = [ 'mango', 'banana', 'orange', 'apple'],
+number = [1, 2, 3, 4, 5, 6, 7, 8]
+students = [
+   student1 = {
+      studentName: 'nonso',
+      class: 'jss1',
+      age: '12',
+      gender: 'male',
+   },
+   student1 = {
+      studentName: 'testimony',
+      class: 'jss1',
+      age: '9',
+      gender: 'female',
+   },
+   student1 = {
+      studentName: 'buchi',
+      class: 'jss3',
+      age: '15',
+      gender: 'male',
+   },
+
+]
+//array methods
+let value 
+ //let value = fruits.toString()
+ //let value = fruits.unshift('grape')
+//value = fruits.shift('grape')
+//value = fruits.pop()
+//value = fruits.push('grape')
+//value = fruits.slice(1,2)
+//value = fruits.splice(0,2, 'grape', 'watermelon')
+ //value = fruits.concat(number)
+ //fruits[0] = 'grape'
+//console.log(fruits)
+// oprators and functions
---
+++ lesson.js
@@ -0,0 +1,130 @@
+//variables and primitives
+// var
+// let
+// const 
+var x = 10
+x = 'benji'
+let y = 20
+y = 30
+const z = 40
+console.log(x,y, z);
+// data types 
+// String
+// number
+// Boolean
+// objects
+// arrays
+let bool = 5>6
+console.log(typeof bool,bool)
+let fullName, firstName, lastName
+ fullName = null
+ console.log(typeof fullName, fullName)
+// firstName = 'nonso'
+// lastName = 'buchi'
+// fullName = firstName +' '+ lastName
+// let studentName = 'Dami' 
+// console.log(firstName.trim())
+let num = '3' + 3
+console.log(typeof num, num)
+const studentData = {
+    studentName: 'Ben',
+    age: 15,
+    gender: 'male',
+    Results: {
+        english: 20,
+        maths: 5,
+        Sports: 100,
+    }
+}
+console.log(`my name is ${studentData.studentName}, age is ${studentData.age} and i am a ${studentData.gender}`)
+
+const students = [
+    studentReg = {
+        studentName: 'Ben',
+        age: 15,
+        gender: 'male',
+        Results: {
+            english: 20,
+            maths: 5,
+            Sports: 100,
+        }
+    },
+    studentReg = {
+        studentName: 'Buchi',
+        age: 16,
+        gender: 'male',
+        Results: {
+            english: 50,
+            maths: 50,
+            Sports: 100,
+        }
+    },
+    studentReg = {
+        studentName: 'Caleb',
+        age: 18,
+        gender: 'male',
+        Results: {
+            english: 10,
+            maths: 53,
+            Sports: 10,
+        }
+    },
+    studentReg = {
+        studentName: 'Kaosara',
+        age: 14,
+        gender: 'female',
+        Results: {
+            english: 20,
+            maths: 5,
+            Sports: 100,
+        }
+    }
+]
+console.log(students[3].age)
+const fruits = ['orange', 'mango', 'watermelon', 'grapes']
+const numb = [1,2,3,4,5]
+// console.log(fruits.length)
+// console.log(fruits.toString())
+// console.log(fruits.pop());
+// console.log(fruits)
+// console.log(fruits.push('apple'))
+// console.log(fruits)
+// console.log(fruits.shift())
+// console.log(fruits)
+// console.log(fruits.unshift('apple'))
+// console.log(fruits)
+// console.log(fruits.slice(3))
+// console.log(fruits.splice(2, 1, 'apple'))
+// console.log(fruits)
+// console.log(fruits.concat(numb))
+
+//javascript operators
+// assign values
+// compare values
+// perform arithmetics 
+let student = {
+
+}
+let a, b, result
+a = 5
+b = 5
+// result = a + b
+// result = b - a
+// result = a * b
+// result = b / a
+// b++
+// b--
+// b -= 5
+// b -= 5
+//  result = a === b
+// result =  a <= b
+// result =  a >= b
+// result =  a !== b
+result =  a > b
+
+ function add() {
+    return result = a + b
+ }
+ result = add()
+ console.log(result)
+ 
